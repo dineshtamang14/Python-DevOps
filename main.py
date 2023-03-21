d98a951278ee51355d0a84d38b72b50251640f65
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-03-21 14:35:28 +0530

    updated base template

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///todos.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
 class Todo(db.Model):
+    __tablename__ = "todos"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+    complete = db.Column(db.Boolean, default=False)
     
+with app.app_context():
+    db.create_all()
 
 @app.route("/edit")
 def home():
